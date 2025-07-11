f539899dbe736350025d764c97bba1fd1883890c
Eli Ribble <eli@theribbles.org>
2025-07-11 21:23:03 +0000

    Alphabetize options in configuration.nix

--- nixos-anywhere/configuration.nix
+++ nixos-anywhere/configuration.nix
@@ -16,18 +16,19 @@
 		efiSupport = true;
 		efiInstallAsRemovable = true;
 	};
-	services.openssh.enable = true;
 
 	environment.systemPackages = map lib.lowPrio [
 		pkgs.curl
 		pkgs.gitMinimal
 	];
 
+	services.openssh.enable = true;
+
+	system.stateVersion = "25.05";
+
 	users.users.root.openssh.authorizedKeys.keys =
 	[
-		# change this to your ssh key
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvhtF6nRWlA6PVs71Eek7p0p2PxTd3P6ZEGFV2t75MB eliribble@nixos"
 	] ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
 
-	system.stateVersion = "25.05";
 }
