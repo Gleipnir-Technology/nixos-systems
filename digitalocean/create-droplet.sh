f19be411a24f75d607476e6f89fc1c70a2f1df21
Eli Ribble <eli@theribbles.org>
2025-07-17 16:45:41 +0000

    Make create-droplet directly executable, and have slightly better documentation

--- digitalocean/create-droplet.sh
+++ digitalocean/create-droplet.sh
@@ -1,11 +1,20 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+set -xe
+NAME="test.nidus.cloud"
+PROJECT="1782f0e0-0968-4fe2-ac7d-192584ec7ce5"
+REGION="sfo3"
+SIZE="s-1vcpu-2gb"
+#SIZE="s-1vcpu-1gb" \
+SSH_KEYS="48777034,46710608"
+TAGS="nixos,test"
 doctl compute droplet create \
-	"test.nidus.cloud" \
+	$NAME \
 	--enable-ipv6 \
 	--image debian-12-x64 \
-	--project-id ce2159e8-02f5-4169-8943-f34ccf812d23 \
-	--region sfo3 \
-	--size s-1vcpu-2gb \
-	--ssh-keys 48777034,46710608 \
-	--tag-name nixos \
+	--project-id $PROJECT\
+	--region $REGION \
+	--size $SIZE\
+	--ssh-keys $SSH_KEYS\
+	--tag-name $TAGS \
 	--wait
-# --size s-1vcpu-1gb \
