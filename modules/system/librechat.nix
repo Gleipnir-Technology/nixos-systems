6b237aa4f3056c77440dcab486b4b9d7defd0568
Eli Ribble <eli@theribbles.org>
2025-07-22 16:44:40 +0000

    Add meilisearch to librechat
    This allows us to search previous chat messages and get fast results.

--- modules/system/librechat.nix
+++ modules/system/librechat.nix
@@ -6,6 +6,7 @@ with lib;
 	config = mkIf config.myModules.librechat.enable {
 		environment.systemPackages = [
 			pkgs.librechat
+			pkgs.meilisearch
 		];
 		services.caddy.virtualHosts."ai.gleipnir.technology".extraConfig = ''
 			reverse_proxy http://localhost:10050
@@ -20,6 +21,14 @@ with lib;
 			owner = "librechat";
 			restartUnits = ["librechat.service"];
 			sopsFile = ../../secrets/librechat.env;
+		};
+		sops.secrets.meilisearch-env = {
+			format = "dotenv";
+			group = "meilisearch";
+			mode = "0440";
+			owner = "meilisearch";
+			restartUnits = ["meilisearch.service"];
+			sopsFile = ../../secrets/meilisearch.env;
 		};
 		systemd.services.librechat = {
 			after=["network.target" "network-online.target"];
@@ -38,13 +47,35 @@ with lib;
 			};
 			wantedBy = ["multi-user.target"];
 		};
+		systemd.services.meilisearch = {
+			after=["network.target" "network-online.target"];
+			description="Self-hosted LLM chat search";
+			documentation=["https://www.meilisearch.com/docs/learn/self_hosted/configure_meilisearch_at_launch"];
+			requires=["network-online.target"];
+			serviceConfig = {
+				EnvironmentFile="/var/run/secrets/meilisearch-env";
+				Type = "simple";
+				User = "meilisearch";
+				Group = "meilisearch";
+				ExecStart = "${pkgs.meilisearch}/bin/meilisearch";
+				TimeoutStopSec = "5s";
+				PrivateTmp = true;
+				WorkingDirectory = "/opt/meilisearch";
+			};
+			wantedBy = ["multi-user.target"];
+		};
 		systemd.tmpfiles.rules = [
 			"d /opt/librechat 0755 librechat librechat"
+			"d /opt/meilisearch 0755 meilisearch meilisearch"
 		];
 		users.groups.librechat = {};
+		users.groups.meilisearch = {};
 		users.users.librechat = {
 			group = "librechat";
-			isNormalUser = false;
+			isSystemUser = true;
+		};
+		users.users.meilisearch = {
+			group = "meilisearch";
 			isSystemUser = true;
 		};
 
