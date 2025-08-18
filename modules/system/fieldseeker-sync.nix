ba87117ed32d2769fc1ace9d78db59bb6b33a367
Eli Ribble <eli@gleipnir.technology>
2025-08-18 20:29:31 +0000

    Reference a built binary in fieldseeker-sync, add initial service
    This won't work yet as we don't read from the environment variables
    and are missing a complete config file

--- modules/system/fieldseeker-sync.nix
+++ modules/system/fieldseeker-sync.nix
@@ -1,18 +1,48 @@
 { pkgs, lib, config, ... }:
 with lib;
 let
-	src = pkgs.fetchFromGitHub {
+	src = pkgs.callPackage (pkgs.fetchFromGitHub {
 		owner  = "Gleipnir-Technology";
 		repo   = "fieldseeker-sync";
-		rev    = "5d19ceb020cf74327c966fabae51e0e1cdd7bd70";
-		sha256 = "sha256-J2k3a960TT31eNp77wKUGJJZSaEYI9ENyVgxxY/RMls=";
-  	};
+		rev    = "2aa2d37e1ccd9471d332d36042fb0b1edd89d08f";
+		sha256 = "sha256-Sa69TONC+EJW9/SmbrimJptnqmxQc1uh2NfY9UuD7e0=";
+  	}) { };
 in {
 	options.myModules.fieldseeker-sync.enable = mkEnableOption "custom fieldseeker-sync configuration";
 
 	config = mkIf config.myModules.fieldseeker-sync.enable {
 		environment.systemPackages = [
-			(pkgs.callPackage src { })
+			src
 		];
+		services.postgresql.enable = true;
+		sops.secrets.fieldseeker-sync-env = {
+			format = "dotenv";
+			group = "fieldseeker-sync";
+			mode = "0440";
+			owner = "fieldseeker-sync";
+			restartUnits = ["fieldseeker-sync.service"];
+			sopsFile = ../../secrets/fieldseeker-sync.env;
+		};
+		systemd.services.fieldseeker-sync = {
+			after=["network.target" "network-online.target"];
+			description="FieldSeeker sync";
+			requires=["network-online.target"];
+			serviceConfig = {
+				EnvironmentFile="/var/run/secrets/fieldseeker-sync-env";
+				Type = "simple";
+				User = "fieldseeker-sync";
+				Group = "fieldseeker-sync";
+				ExecStart = "${src}/bin/full-export";
+				TimeoutStopSec = "5s";
+				PrivateTmp = true;
+				WorkingDirectory = "/tmp";
+			};
+			wantedBy = ["multi-user.target"];
+		};
+		users.groups.fieldseeker-sync = {};
+		users.users.fieldseeker-sync = {
+			group = "fieldseeker-sync";
+			isSystemUser = true;
+		};
 	};
 }
