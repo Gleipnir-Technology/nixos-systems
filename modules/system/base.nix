d5403cc9fbc5b1c5f42994ff37f5077b6faca235
Eli Ribble <eli@gleipnir.technology>
2025-08-22 22:13:33 +0000

    Add binutils to all servers
    It gives me access to 'strings' which allows me to debug environment
    variables of running programs

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -6,7 +6,11 @@
 
 	boot.tmp.cleanOnBoot = true;
 	environment.systemPackages = map lib.lowPrio [
+<<<<<<< HEAD
 		pkgs.cloud-init
+=======
+		pkgs.binutils
+>>>>>>> 3563d6b (Add binutils to all servers)
 		pkgs.curl
 		pkgs.dig
 		pkgs.htop
