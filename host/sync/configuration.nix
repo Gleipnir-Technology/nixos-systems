8b669fe507aced94c12a4c150e8d685b0388a2c2
Eli Ribble <eli@gleipnir.technology>
2025-08-23 18:33:57 +0000

    Add firewall configuration to sync

--- host/sync/configuration.nix
+++ host/sync/configuration.nix
@@ -4,4 +4,22 @@
 	];
 	myModules.caddy.enable = true;
 	myModules.fieldseeker-sync.enable = true;
+	networking = {
+		firewall = {
+			enable = true;
+			interfaces.ens3 = {
+				allowedUDPPorts = [ 22 80 443 ];
+				allowedTCPPorts = [ 22 80 443 ];
+			};
+			trustedInterfaces = [
+				"ens4"
+				"lo"
+			];
+		};
+		networkmanager.enable = false;
+		nftables = {
+			enable = true;
+		};
+		useNetworkd = true;
+	};
 }
