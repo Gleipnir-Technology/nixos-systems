29fd7815d837c8b38c9ecd012ae39e6b30d2e0c9
Eli Ribble <eli@gleipnir.technology>
2025-09-08 21:24:28 +0000

    Remove old nocix experimental configs, solidify new corp configuration
    At this point the new Nocix server is working (I'm writing this commit
    on that machine) so I can clean up the various experiments and commit to
    the more generalized configuration as it stands

--- host/corp/configuration.nix
+++ host/corp/configuration.nix
@@ -59,5 +59,5 @@
 	# and migrated your data accordingly.
 	#
 	# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-	system.stateVersion = "23.11";
+	#system.stateVersion = "23.11";
 }
