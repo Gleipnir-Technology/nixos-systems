bbc2bbb5e1eb8c4143d3494f705ffb5eff135e3c
Eli Ribble <eli@theribbles.org>
2025-09-29 22:07:43 +0000

    Break networking out to its own file.
    This makes it easier to figure out what needs review and changing
    Also switch to the latest detected hardware state.

--- nixos-anywhere/nocix/configuration.nix
+++ nixos-anywhere/nocix/configuration.nix
@@ -9,6 +9,7 @@
 		(modulesPath + "/installer/scan/not-detected.nix")
 		(modulesPath + "/profiles/qemu-guest.nix")
 		./disk-config.nix
+		./network.nix
 	];
 	boot.loader.grub = {
 		# no need to set devices, disko will add all devices that have a EF02 partition to the list already
@@ -21,28 +22,6 @@
 		pkgs.dig
 		pkgs.gitMinimal
 	];
-	networking = {
-		defaultGateway = {
-			address = "107.150.59.201";
-			interface = "enp2s0";
-		};
-		defaultGateway6 = {
-			address = "2604:4300:a:27e::1";
-			interface = "enp2s0";
-		};
-		interfaces.enp2s0 = {
-			ipv4.addresses = [{
-				address = "107.150.59.202";
-				prefixLength = 29;
-			}];
-			ipv6.addresses = [{
-				address = "2604:4300:a:27e::2";
-				prefixLength = 64;
-			}];
-		};
-		nameservers = ["192.187.107.16"];
-		search = ["nocix.net"];
-	};
 	services.openssh.enable = true;
 	users.users.root.openssh.authorizedKeys.keys =
 	[
---
+++ nixos-anywhere/nocix/network.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+{
+	networking = {
+		defaultGateway = {
+			address = "63.141.227.153";
+			interface = "enp3s0";
+		};
+		defaultGateway6 = {
+			address = "2604:4300:a:88::1";
+			interface = "enp3s0";
+		};
+		interfaces.enp2s0 = {
+			ipv4.addresses = [{
+				address = "63.141.227.154";
+				prefixLength = 29;
+			}];
+			ipv6.addresses = [{
+				address = "2604:4300:a:88::2";
+				prefixLength = 64;
+			}];
+		};
+		nameservers = ["8.8.8.8"];
+		search = ["nocix.net"];
+	};
+}
