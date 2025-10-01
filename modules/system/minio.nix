938b1863f4a7e2bba3140118b485227fad899911
Eli Ribble <eli@gleipnir.technology>
2025-10-01 21:41:20 +0000

    Fix missing redirect for console login
    Without this we get redirected to "/console" which doesn't load the UI
    for some reason.

--- modules/system/minio.nix
+++ modules/system/minio.nix
@@ -4,6 +4,7 @@ with lib;
 	options.myModules.minio.enable = mkEnableOption "custom minio configuration";
 	config = mkIf config.myModules.minio.enable {
 		services.caddy.virtualHosts."s3.gleipnir.technology".extraConfig = ''
+			redir /console /console/
 			handle_path /console* {
 				reverse_proxy http://localhost:10081
 			}
