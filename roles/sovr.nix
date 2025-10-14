1119c3668cd706b9773e2388531e1ec7eec368d5
Eli Ribble <eli@gleipnir.technology>
2025-10-14 18:32:45 +0000

    Flesh out all my ribbles domains

--- roles/sovr.nix
+++ roles/sovr.nix
@@ -2,6 +2,24 @@
 	myModules.caddy.enable = true;
 	myModules.frps = {
 		enable = true;
-		subdomains = ["plex"];
+		subdomains = [
+			"audiobooks"
+			"auth"
+			"carddav"
+			"chores"
+			"collabora"
+			"docs"
+			"files"
+			"home-assistant"
+			"movies"
+			"notes"
+			"passwords"
+			"pdf"
+			"photos"
+			"plex"
+			"source"
+			"todo"
+			"tv"
+		];
 	};
 }
