bdd58efc4e842fc8c781d005241634f6124a3b5c
Eli Ribble <eli@gleipnir.technology>
2025-12-16 03:41:36 +0000

    Add old laptop key

--- users/eliribble.nix
+++ users/eliribble.nix
@@ -26,6 +26,7 @@ with lib;
 		openssh.authorizedKeys.keys = [
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvhtF6nRWlA6PVs71Eek7p0p2PxTd3P6ZEGFV2t75MB eliribble@nixos"
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHL1SpT3KR8XeXtH19muncYVrKxWzWdWtJYNTwoJGTm3 eliribble@Elis-Mac-mini.local"
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIS/3a3sZtv54V3jZdXdb9tKR0B1joSv0pRPpdsUHI0+ eliribble@daevas"
 		];
 	};
 }
