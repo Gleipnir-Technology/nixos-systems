0dd71de7a6890eb37a6e840cda1007a8f95c6f5a
Eli Ribble <eli@theribbles.org>
2026-01-05 14:07:23 +0000

    Remove fss-gleipnir-qa
    We'll be switching it to the new nidus-sync project soon.

--- roles/nidus-sync.nix
+++ roles/nidus-sync.nix
@@ -8,20 +8,11 @@ let
 		subdomain = "deltamvcd";
 		inherit lib pkgs;
 	};
-	fss-gleipnir-qa = import ../modules/system/fieldseeker-sync.nix {
-		customer = "gleipnir-qa";
-		dataDirectory = /mnt/bigdisk/fieldseeker-sync/gleipnir-qa;
-		fieldseeker-sync = inputs.fieldseeker-sync;
-		port = 3001;
-		subdomain = "gleipnir-qa";
-		inherit lib pkgs;
-	};
 	nidus-name-dev = "nidus-dev-sync";
 in {
-	environment = pkgs.lib.mkMerge [ fss-deltamvcd.environment fss-gleipnir-qa.environment ];
+	environment = pkgs.lib.mkMerge [ fss-deltamvcd.environment ];
 	services = pkgs.lib.mkMerge [
 		fss-deltamvcd.services
-		fss-gleipnir-qa.services
 
 		{
 			caddy.virtualHosts."dev-sync.nidus.cloud".extraConfig = ''
@@ -45,7 +36,6 @@ in {
 	];
 	sops = pkgs.lib.mkMerge [
 		fss-deltamvcd.sops
-		fss-gleipnir-qa.sops  
 		{
 			secrets."nidus-dev-sync-env" = {
 				format = "dotenv";
@@ -57,10 +47,9 @@ in {
 			};
 		}
 	];
-	systemd = pkgs.lib.mkMerge [ fss-deltamvcd.systemd fss-gleipnir-qa.systemd ];
+	systemd = pkgs.lib.mkMerge [ fss-deltamvcd.systemd ];
 	users = pkgs.lib.mkMerge [
 		fss-deltamvcd.users
-		fss-gleipnir-qa.users
 
 		{
 			groups."${nidus-name-dev}" = {};
